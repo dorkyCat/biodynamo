f281d805878b76900a89e2e99d349c0dee7585a3
Ahmad Hesam <hesam212@gmail.com>
2020-08-05 18:11:38 +0200

    WIP

--- src/core/execution_context/in_place_exec_ctxt.cc
+++ src/core/execution_context/in_place_exec_ctxt.cc
@@ -322,7 +322,6 @@ void InPlaceExecutionContext::ForEachNeighborWithinRadius(
   // forward call to env and populate cache
   auto* env = Simulation::GetActive()->GetEnvironment();
 
-  std::cout << "SQUARED RADUS = " << squared_radius << std::endl;
   ForEachNeighborWithinRadiusFunctor for_each(lambda, neighbor_cache_,
                                               squared_radius);
   env->ForEachNeighbor(for_each, query);
