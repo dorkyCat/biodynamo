8d61aa6eb5e88157767ea1655c53bff0e4c3b7ed
Dorukhan Arslan <dorukhan.ars.css@gmail.com>
2021-06-24 01:21:13 +0200

    get list of all dependencies

--- util/git/actions_util/ci-info.sh
+++ util/git/actions_util/ci-info.sh
@@ -14,13 +14,17 @@
 # -----------------------------------------------------------------------------
 
 # This script is used to sample build/environment related information from our
-# GitHub Actions runs. This information helps us determine the side-effects of 
-# installing and using BioDynaMo.
+# GitHub Actions runs. This information helps us determine the side-effects of
+# installing and using BioDynaMo, and to create build recipes/guides.
 
-XML_OUT="$HOME/ci-run-info.xml"
-DIGIT_PAT='[[:digit:]]+.[[:digit:]]+.[[:digit:]]+'
+_XML_OUT="$HOME/ci-run-info.xml"
+_DIGIT_PAT='[[:digit:]]+.[[:digit:]]+.[[:digit:]]+'
 
-# Description: Wrap string or stdin in given XML tag, append to XML_OUT
+function _err {
+  printf "%.23s %s[%s]: %s\n" $(date +%F.%T.%N) ${BASH_SOURCE[1]##*/} ${BASH_LINENO[0]} "${@}"
+}
+
+# Description: Wrap string or stdin in given XML tag, append to _XML_OUT
 # Usage: Tagged <tag-name> <str-or-stdin>
 function Tagged {
   # pattern source: https://stackoverflow.com/a/12873723/13380539
@@ -38,63 +42,91 @@ function Tagged {
   else # 2nd string arg
     res=$(sed "$escpat" <<<"$2")
   fi
-  echo "<$tag>$res</$tag>" >>"$XML_OUT"
+  echo "<$tag>$res</$tag>" >>"$_XML_OUT"
   return $retc
 }
 
-# Collect info BEFORE thisdbm using commands common to most GNU/Linux systems
+# Collect info BEFORE thisbdm using  
+# commands common to most GNU/Linux systems
 function CommonLinuxPreBdmInfoDump {
   uname -a | Tagged 'uname'
   Tagged 'os-release' </etc/os-release
-  ( set -o posix; set ) | Tagged 'environment-pre-bdm'
+  (
+    set -o posix
+    set
+  ) | Tagged 'environment-pre-bdm'
   lsmod | Tagged 'modules-pre-bdm'
 }
 
-# Collect info AFTER thisdbm
+function CommonLinuxPyenvSetup {
+  export PATH="$HOME/.pyenv/bin:$PATH"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)"
+  pyenv shell 3.9.1
+  which python3
+}
+
+# Collect info AFTER thisbdm
+# Precondition: inside biodynamo build directory
 function CommonLinuxBdmInfoDump {
+  . bin/thisbdm.sh
   lsmod | Tagged 'modules-bdm'
   cmake --graphviz=dep.dot . && cat dep.dot | Tagged 'dependency-graph'
   rm dep.dot
-  cmake --version | head -n 1 | grep -Eo "$DIGIT_PAT" | Tagged 'cmake-version'
+  cmake --version | head -n 1 | grep -Eo "$_DIGIT_PAT" | Tagged 'cmake-version'
   cmake -LA -N . | awk '{if(f)print} /-- Cache values/{f=1}' | Tagged 'cmake-build-environment'
   biodynamo --version | Tagged 'bdm-version'
   mpiexec --version | head -n 1 | Tagged 'mpi-version'
   g++ --version | head -n 1 | Tagged 'compiler-version'
-  python3 --version | grep -Eo "$DIGIT_PAT" | Tagged 'python3-version'
+  python3 --version | grep -Eo "$_DIGIT_PAT" | Tagged 'python3-version'
   pip3 list --format=freeze --disable-pip-version-check | Tagged 'pip-packages'
 }
 
+# $1 os tag name attribute
 function CompleteDumpXML {
-  # $1 is os name attribute
   (
     echo '<?xml version="1.0"?>'
     echo "<os name='$1'>"
-    cat "$XML_OUT"
+    cat "$_XML_OUT"
     echo '</os>'
-  ) >"${XML_OUT}.bak"
-  rm "$XML_OUT" && mv "${XML_OUT}.bak" "$XML_OUT"
+  ) >"${_XML_OUT}.bak"
+  rm "$_XML_OUT" && mv "${_XML_OUT}.bak" "$_XML_OUT"
 }
 
 function Centos7InfoDumpInit {
-  touch "$XML_OUT"
+  touch "$_XML_OUT"
   yum list installed | Tagged 'packages-pre-bdm'
 }
 
 function Centos7InfoDump {
   CommonLinuxPreBdmInfoDump
-  . scl_source enable devtoolset-8 || echo 'skip'
-  . /etc/profile.d/modules.sh || echo 'skip'
-  module load mpi || echo 'skip'
-  export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-  pyenv shell 3.9.1
+  . scl_source enable devtoolset-8 || _err 'skip'
+  . /etc/profile.d/modules.sh || _err 'skip'
+  module load mpi || _err 'skip'
+  CommonLinuxPyenvSetup
   export DISPLAY=:99.0
   sleep 3
-  . bin/thisbdm.sh
   CommonLinuxBdmInfoDump
   # RPM specific commands
   yum list installed | Tagged 'packages-bdm'
+  bash -c 'cat << EOF  > /etc/yum.repos.d/springdale-7-SCL.repo
+[SCL-core]
+name=Springdale SCL Base 7.6 - x86_64
+mirrorlist=http://springdale.princeton.edu/data/springdale/SCL/7.6/x86_64/mirrorlist
+#baseurl=http://springdale.princeton.edu/data/springdale/SCL/7.6/x86_64
+gpgcheck=1
+gpgkey=http://springdale.math.ias.edu/data/puias/7.6/x86_64/os/RPM-GPG-KEY-puias
+EOF'
+  yum check-update
   yum repolist | Tagged 'repos-bdm'
+  yum list -q -t -y available centos-release-scl epel-release wget \
+    libXt-devel libXext-devel devtoolset-8-gcc* numactl-devel openmpi3-devel \
+    freeglut-devel git @development zlib-devel bzip2 bzip2-devel \
+    readline-devel sqlite sqlite-devel openssl-devel xz xz-devel libffi-devel \
+    findutils doxygen graphviz valgrind freeglut-devel libxml2-devel \
+    llvm-toolset-7 llvm-toolset-7-clang-tools-extra llvm-toolset-7-llvm-devel \
+    llvm-toolset-7-llvm-static gdl-devel atlas-devel blas-devel lapack-devel |
+    grep -E '(.noarch|.x86_64)' | Tagged 'package-manager-all-deps'
+  # done
   CompleteDumpXML 'centos-7'
 }
